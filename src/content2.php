c8b5a5dfa0306952c7fcb1981e4569258cc6fd10
David Skeen <skeenda@onid.oregonstate.edu>
2015-05-03 22:28:09 -0700

    works for the most part

--- src/content2.php
+++ src/content2.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start();
 
+
 //If permission is not given then return to the login page
 if($_GET['login'] != true){
 	header("Location: login.php");
