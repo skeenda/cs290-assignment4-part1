6fe66999f3f0ee1efbfac0c13726c201c2c8e81d
David Skeen <skeenda@onid.oregonstate.edu>
2015-05-03 22:29:39 -0700

    Finished

--- src/multtable.php
+++ src/multtable.php
@@ -3,7 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display', 1);
 
-
 $minMultiplicand = intval($_GET['min-multiplicand']);
 $maxMultiplicand = intval($_GET['max-multiplicand']);
 $minMultiplier = intval($_GET['min-multiplier']);
